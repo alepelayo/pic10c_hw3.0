893b9045ea47be74bb27e2ffc895cdb6cee5038c
alepelayo <alepelayo@g.ucla.edu>
2018-06-14 11:58:34 -0700

    deleting Qt stuff

--- pic10c_homework3/ring_queue.cpp
+++ pic10c_homework3/ring_queue.cpp
@@ -1,17 +1,3 @@
-/*
-#include <QCoreApplication>
-#include <QDebug>
-
-
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-
-    qDebug () << "hello world";
-    return a.exec();
-}
-*/
-
 #include <iostream>
 
 // Forward declaration
